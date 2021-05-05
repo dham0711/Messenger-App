f6b669853f7c6231ca9ba7082049cf9871dd3443
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-05-05 15:57:37 -0500

    folder add. still working on routes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { User } = require('../../models/User');
+
+// This route uses async/await with '.catch()' for errors
+
+router.get('/', async (req, res) => {
+  // find all categories
+  
+  try {
+
+    const userData = await User.findAll();
+    res.status(200).json(userSeedData);
+
+  } catch(err) {
+
+    res.status(500).json(err)
+  }
+  
+
+});
+
+// This route uses async/await with try/catch for errors
+
+router.post('/', async (req, res) => {
+  try {
+    const UserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,1 @@
-const user = require('./user');
 
-// We package our two models and export them as an object so we can import them together and use their proper names
-module.exports = { user };
