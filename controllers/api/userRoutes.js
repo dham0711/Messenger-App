2b44114ab023e18d3f0af221db2eea8ce2967bbc
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-05-07 13:42:45 -0500

    merged our test javascript into server.js/ whole app can run off of port 3001 now

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,15 +21,16 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  console.log(req)
   try {
-    console.log("got inside of try")
 
     const userData = await User.create(req.body);
     res.status(200).json(userData);
+
   } catch (err) {
+
     console.log(err)
     res.status(400).json(err);
+    
   }
 });
 
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const path = require('path');
 const sequelize = require('./config/connection');
-
-// const bp = require('body-parser')
-
-
+const http = require('http');
+const { Server } = require("socket.io");
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,8 +20,38 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers'));
 
-sequelize.sync({ force: false }).then(() => {
+//sockit.io start
 
-  app.listen(PORT, () => console.log('zane is a dummy'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/test/index.html');
+});
+
+io.on('connection', (socket) => {
+
+        console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+// I switched out the server call to connect to the socket.io client. keeping the below code around juuuust incase it causes an issue. which is most certainly will.
+
+// sequelize.sync({ force: false }).then(() => {
+
+//   app.listen(PORT, () => console.log('listening on ' + PORT));
   
+// });
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
 });
