91335ecae1569535871976c7ed4ed190c8465bd9
Devante <devante_hampton@yahoo.com>
2021-05-08 08:43:16 -0500

    add the test layout

---
+++ test/layout/practice.js
@@ -0,0 +1,26 @@
+//String - Hello my name is line 7 and 11 are string variables
+//char - line 12 is a character/char variable
+//Boolean - True or False
+//int
+//float
+//double
+var myVariable = "Hello World";//Initalize variable-Initalize means to give a varialble a name and value
+myVariable = 5;
+var testScore=80 // this a number
+var secondTestScore=65.5
+var greeting="hi how can I help?" //this a string 
+var isOver18=true //this a boolean
+var sum=testScore+secondTestScore
+var dif=testScore-secondTestScore
+var product=testScore*secondTestScore
+var quotient=testScore/secondTestScore
+var n1=37
+var n2=15
+var remainder=n1%n2 //mod function, finds the reminder of n1 divided by n2
+var firstName="john"
+var lastName="smith"
+var fullName=firstName+" "+lastName
+var lengthOfFirstName=firstName.length
+var initials=firstName[3]+"."+lastName[4]+"."
+console.log(initials)
+console.log(remainder)
