e22fb9cb9f18b11b497e10a8464704cbb225ac02
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-05-13 14:34:07 -0500

    config revert

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const PORT = process.env.PORT || 3306;
 
 require('dotenv').config();
 
@@ -12,7 +11,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
-    port: PORT
+    port: 3306
   });
 }
 
