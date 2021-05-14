23f0d25dc4c5004d40dfed82050c93d2d17036f2
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-05-14 12:46:20 -0500

    server fixing

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
